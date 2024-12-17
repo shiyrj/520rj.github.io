d49212ed7f6184e0591afb792315538ef99e5952
shijh <shijianghua17@gmail.com>
2024-12-17 23:50:53 +0800

    111

--- js/hm.js
+++ js/hm.js
@@ -8,10 +8,27 @@ const data = [
     { name: 'Adobe 2020全家桶', link1: 'https://pan.xunlei.com/s/VOEIUacX8YakUoKzRvHyRIF4A1?pwd=tccz#', link2: '#' },
     { name: 'Adobe 2019全家桶', link1: 'https://pan.xunlei.com/s/VOEIUklzzkZkQ432H1zRQPOLA1?pwd=hiwi#', link2: '#' },
     { name: 'Adobe 2018全家桶', link1: 'https://pan.xunlei.com/s/VOEIUqeBRLgaEjUv-8JqOjjNA1?pwd=828j#', link2: '#' },
-    { name: 'Photoshop 2025', link1: 'https://pan.xunlei.com/s/VOEIVCEPkeaC4Rnt4ItiPv8BA1?pwd=aeum#', link2: '#' },
-    { name: 'Photoshop 2024', link1: 'https://pan.xunlei.com/s/VOEIVHVMHtBWt6akwBrXCGBKA1?pwd=x2kq#', link2: '#' },
-    { name: 'Photoshop 2023', link1: 'https://pan.xunlei.com/s/VOEIVMwJf47rQvePICwLpUIIA1?pwd=kvbe#', link2: '#' },
-    { name: 'Photoshop 2022', link1: 'https://pan.xunlei.com/s/VOEIVR_8imJ5xX98QGS_QvBoA1?pwd=innc#', link2: '#' },
+    { name: 'Photoshop(PS)2025', link1: 'https://pan.xunlei.com/s/VOEIVCEPkeaC4Rnt4ItiPv8BA1?pwd=aeum#', link2: '#' },
+    { name: 'Photoshop(PS)2024', link1: 'https://pan.xunlei.com/s/VOEIVHVMHtBWt6akwBrXCGBKA1?pwd=x2kq#', link2: '#' },
+    { name: 'Photoshop(PS)2023', link1: 'https://pan.xunlei.com/s/VOEIVMwJf47rQvePICwLpUIIA1?pwd=kvbe#', link2: '#' },
+    { name: 'Photoshop(PS)2022', link1: 'https://pan.xunlei.com/s/VOEIVR_8imJ5xX98QGS_QvBoA1?pwd=innc#', link2: '#' },
+    { name: 'Photoshop(PS)2021', link1: 'https://pan.xunlei.com/s/VOEK9cXtGmf-20uxzOzN1KpkA1?pwd=u7qn#', link2: '#' },
+    { name: 'Photoshop(PS)2020', link1: 'https://pan.xunlei.com/s/VOEK9g0HaMAPYfBmNUWDNnm8A1?pwd=gwiq#', link2: '#' },
+    { name: 'Photoshop(PS)2019', link1: 'https://pan.xunlei.com/s/VOEK9jwsaMAPYfBmNUWDNqlgA1?pwd=ijj6#', link2: '#' },
+    { name: 'Photoshop(PS)2018', link1: 'https://pan.xunlei.com/s/VOEK9muTDOAkeHJQzQaYukUWA1?pwd=wdj4#', link2: '#' },
+    { name: 'Photoshop(PS)2017', link1: 'https://pan.xunlei.com/s/VOEK9pwq8HiN-rT80AMboCj6A1?pwd=rp64#', link2: '#' },
+    { name: 'Photoshop(PS)CS6', link1: 'https://pan.xunlei.com/s/VOEK9v-jwj8BlTmeR44wVdI7A1?pwd=wpds#', link2: '#' },
+    { name: 'Premiere(PR)2025', link1: 'https://pan.xunlei.com/s/VOEKBRIwhhPtuR3NAlMDDGNcA1?pwd=9e5n#', link2: '#' },
+    { name: 'Premiere(PR)2024', link1: 'https://pan.xunlei.com/s/VOEKDzle1lSI7evBd1_k7iYcA1?pwd=dkuh#', link2: '#' },
+    { name: 'Premiere(PR)2023', link1: 'https://pan.xunlei.com/s/VOEKESFbwj8BlTmeR44wYcf7A1?pwd=y2p5#', link2: '#' },
+
+
+    { name: 'AutoCAD 2025', link1: 'https://pan.xunlei.com/s/VOEKFQC1_k89bkSSS_5wMFPjA1?pwd=dguj#', link2: '#' },
+    { name: 'AutoCAD 2024', link1: 'https://pan.xunlei.com/s/VOEKF_Ryr1lcxE2OFLL9gUDpA1?pwd=i47z#', link2: '#' },
+
+    { name: 'WinRAR', link1: 'https://pan.quark.cn/s/f575bc74c42c', link2: '#' },
+    { name: 'Bandizip', link1: 'https://pan.quark.cn/s/fd768678a466', link2: '#' },
+    { name: '7-zip', link1: 'https://pan.quark.cn/s/1ce8ccfb6d2f', link2: '#' },
     // ... 更多数据项
 ];
 
